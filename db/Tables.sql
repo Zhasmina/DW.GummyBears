12518bb1f917e77f8bfc85a09d8dea3152efd36f
Zhasmina <zhasmina.mitkova@bedegaming.com>
2015-10-24 12:31:24 +0300

    gitignore added.

---
+++ db/Tables.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE GummyBears
+
+GO
+
+CREATE TABLE [dbo].[Users]
+(
+	[Id] [int] NOT NULL PRIMARY KEY IDENTITY,
+	[UserName] [nvarchar](100) NOT NULL,
+	[FirstName] [nvarchar](100) NOT NULL,
+	[LastName] [nvarchar](100) NOT NULL,
+	[Password] [nvarchar](100) NOT NULL,
+	[Email] [nvarchar](100) NOT NULL,
+	[TelephoneNumber] [nvarchar](100) NULL,
+	[DateOfBirth] DATETIME NULL,
+	[Description] [nvarchar](MAX) NULL,
+	[Country] [nvarchar](100) NULL,
+	[ProfilePicturePath] [nvarchar](MAX) NULL
+)
+
+GO
