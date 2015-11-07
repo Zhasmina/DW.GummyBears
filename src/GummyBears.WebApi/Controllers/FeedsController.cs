8bb43610478153b62b55bc8acb3c7cffddc46b90
Zhasmina <zhasmina.mitkova@bedegaming.com>
2015-11-07 18:15:36 +0200

    FeedsController

---
+++ src/GummyBears.WebApi/Controllers/FeedsController.cs
@@ -0,0 +1,18 @@
+﻿using GummyBears.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GummyBears.WebApi.Controllers
+{
+    public class FeedsController : BaseController
+    {
+        //Endpoints to manage messages in feed
+        public FeedsController(IDbContext dbContext)
+            : base(dbContext)
+        {
+        }
+
+    }
+}
