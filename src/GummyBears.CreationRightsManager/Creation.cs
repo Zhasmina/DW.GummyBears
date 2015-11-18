1f2c5276fed4d573718054b2388d929d512d5e6c
Dragomir Draganov <d.draganov@hotmail.com>
2015-11-19 00:00:55 +0200

    unstable

---
+++ src/GummyBears.CreationRightsManager/Creation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace GummyBears.CreationRightsManager
+{
+    public class Creation
+    {
+        public string Author { get; set; }
+
+        public string Owner { get; set; }
+
+        public DateTime TimeOfCreation { get; set; }
+
+        public MemoryStream Data { get; set; }
+
+        public string ToMetaString()
+        {
+            RIPEMD160 myRIPEMD160 = RIPEMD160.Create();
+            // Compute the hash of the fileStream.
+            var hashValue = myRIPEMD160.ComputeHash(Data);
+            return string.Format("{0}/{1}/{2}/{3}", Author, Owner, TimeOfCreation.Ticks, Convert.ToBase64String(hashValue));
+        }
+    }
+}
---
+++ src/GummyBears.CreationRightsManager/Manager.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace GummyBears.CreationRightsManager
+{
+    public class Manager
+    {
+        private string _cryptoKey;
+        private string _cryproSalt;
+        private string _cryptoSymetricKey;
+
+        public Manager()
+        {
+            _cryptoKey = ConfigurationManager.AppSettings["CreationRightsManager.CryptoKey"];
+            _cryproSalt = ConfigurationManager.AppSettings["CreationRightsManager.CryptoSalt"];
+            _cryptoSymetricKey = ConfigurationManager.AppSettings["CreationRightsManager.CryptoReadOnlyKey"];
+        }
+
+        public CreationCertificateData Register(Creation creation)
+        {
+            string metaString = creation.ToMetaString();
+            CreationCertificateData result = new CreationCertificateData
+            {
+
+            };
+        }
+
+        public string Encrypt(string plainText)
+        {
+            byte[] plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+
+            byte[] keyBytes = new Rfc2898DeriveBytes(_cryptoKey, Encoding.ASCII.GetBytes(_cryproSalt)).GetBytes(256 / 8);
+            var symmetricKey = new RijndaelManaged() { Mode = CipherMode.CBC, Padding = PaddingMode.Zeros };
+            var encryptor = symmetricKey.CreateEncryptor(keyBytes, Encoding.ASCII.GetBytes(_cryptoSymetricKey));
+
+            byte[] cipherTextBytes;
+
+            using (var memoryStream = new MemoryStream())
+            {
+                using (var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
+                {
+                    cryptoStream.Write(plainTextBytes, 0, plainTextBytes.Length);
+                    cryptoStream.FlushFinalBlock();
+                    cipherTextBytes = memoryStream.ToArray();
+                    cryptoStream.Close();
+                }
+                memoryStream.Close();
+            }
+            return Convert.ToBase64String(cipherTextBytes);
+        }
+
+        public string Decrypt(string encryptedText)
+        {
+            byte[] cipherTextBytes = Convert.FromBase64String(encryptedText);
+            byte[] keyBytes = new Rfc2898DeriveBytes(_cryptoKey, Encoding.ASCII.GetBytes(_cryproSalt)).GetBytes(256 / 8);
+            var symmetricKey = new RijndaelManaged() { Mode = CipherMode.CBC, Padding = PaddingMode.None };
+
+            var decryptor = symmetricKey.CreateDecryptor(keyBytes, Encoding.ASCII.GetBytes(_cryptoSymetricKey));
+            var memoryStream = new MemoryStream(cipherTextBytes);
+            var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read);
+            byte[] plainTextBytes = new byte[cipherTextBytes.Length];
+
+            int decryptedByteCount = cryptoStream.Read(plainTextBytes, 0, plainTextBytes.Length);
+            memoryStream.Close();
+            cryptoStream.Close();
+            return Encoding.UTF8.GetString(plainTextBytes, 0, decryptedByteCount).TrimEnd("\0".ToCharArray());
+        }
+    }
+}
