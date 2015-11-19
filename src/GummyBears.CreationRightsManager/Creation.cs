f8a7cc7c36ae0103de16f3859ac39eaa0d3371c0
Dragomir Draganov <d.draganov@hotmail.com>
2015-11-19 23:46:56 +0200

    data v data footprint

--- src/GummyBears.CreationRightsManager/Creation.cs
+++ src/GummyBears.CreationRightsManager/Creation.cs
@@ -6,15 +6,47 @@ namespace GummyBears.CreationRightsManager
 {
     public class Creation : CreationRightsData
     {
-        public MemoryStream Data { get; set; }
+        private string _dataFootPrint;
+        private MemoryStream _data;
+
+        public MemoryStream Data
+        {
+            private get
+            {
+                return _data;
+            }
+            set
+            {
+
+                if (_dataFootPrint != null)
+                {
+                    throw new Exception("Data should not be set after data footprint.");
+                }
+            }
+        }
 
         public string DataFootprint
         {
             get
             {
-                RIPEMD160 myRIPEMD160 = RIPEMD160.Create();
-                var hash = myRIPEMD160.ComputeHash(Data);
-                return Convert.ToBase64String(hash);
+                if (_dataFootPrint == null)
+                {
+                    RIPEMD160 myRIPEMD160 = RIPEMD160.Create();
+                    var hash = myRIPEMD160.ComputeHash(Data);
+                    _dataFootPrint = Convert.ToBase64String(hash);
+                }
+
+                return _dataFootPrint;
+            }
+
+            set
+            {
+                if (Data != null)
+                {
+                    throw new Exception("If Data is not null, footprint should be calculated, not set");
+                }
+
+                _dataFootPrint = value;
             }
         }
 
