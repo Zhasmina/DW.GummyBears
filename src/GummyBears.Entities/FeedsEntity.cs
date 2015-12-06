2535206818c7d92f2a5a71d43a52907960be4d4f
Dragomir Draganov <d.draganov@hotmail.com>
2015-12-06 20:52:31 +0200

    feeeeeeeds

---
+++ src/GummyBears.Entities/FeedsEntity.cs
@@ -0,0 +1,22 @@
+﻿using Dapperer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GummyBears.Entities
+{
+    [Table("Feeds")]
+    public class FeedsEntity : BaseEntity
+    {
+        [Column("Text")]
+        public string Text { get; set; }
+
+        [Column("AuthorId")]
+        public int AuthorId { get; set; }
+
+        [Column("CreatedAt")]
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ src/GummyBears.Repository/FeedsRepository.cs
@@ -0,0 +1,19 @@
+﻿using Dapperer;
+using Dapperer.DbFactories;
+using GummyBears.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GummyBears.Repository
+{
+    public class FeedsRepository : Repository<FeedsEntity, int>
+    {
+        public FeedsRepository(IQueryBuilder queryBuilder, SqlDbFactory sqlDbFactory)
+            : base(queryBuilder, sqlDbFactory)
+        {
+        }
+    }
+}
