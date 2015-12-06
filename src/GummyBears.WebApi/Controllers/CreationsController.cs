c39014ab0d9f3183fb75613c80d44650b1828cae
Zhasmina <zhasmina.mitkova@bedegaming.com>
2015-12-06 23:35:56 +0200

    CreationsController

---
+++ src/GummyBears.WebApi/Controllers/CreationsController.cs
@@ -0,0 +1,16 @@
+﻿using GummyBears.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GummyBears.WebApi.Controllers
+{
+    public class CreationsController : BaseController
+    {
+        public CreationsController(IDbContext dbContext)
+            :base(dbContext)
+        {
+        }
+    }
+}
