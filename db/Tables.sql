dc142f508f9308a04f756254e2fda9edad6b0d1a
Dragomir Draganov <d.draganov@hotmail.com>
2015-12-06 19:11:01 +0200

    minka sql fix

--- db/Tables.sql
+++ db/Tables.sql
@@ -1,6 +1,11 @@
+
+DROP DATABASE GummyBears
+
 CREATE DATABASE GummyBears
 
 GO
+ 
+USE GummyBears
 
 CREATE TABLE [dbo].[Users]
 (
@@ -15,7 +20,7 @@ CREATE TABLE [dbo].[Users]
 	[Description] [nvarchar](MAX) NULL,
 	[Country] [nvarchar](100) NULL,
 	[ProfilePicturePath] [nvarchar](MAX) NULL,
-	Role [nvarchar(50)] NULL
+	[Role] [nvarchar](50) NULL
 )
 
 GO
@@ -24,3 +29,5 @@ CREATE TABLE [dbo].[Authentications](
 	[Token] [nvarchar](150) NOT NULL PRIMARY KEY,
 	[UserId] [int] NOT NULL  FOREIGN KEY REFERENCES Users(Id),
 	[LastSeen] [datetime] NOT NULL)
+
+GO
