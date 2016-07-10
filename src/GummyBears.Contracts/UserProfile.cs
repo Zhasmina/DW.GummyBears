868318ec14c837694cef7894588196c73d81c021
Dragomir Draganov <d.draganov@hotmail.com>
2016-07-10 15:16:47 +0300

    Fixed regex

--- src/GummyBears.Contracts/UserProfile.cs
+++ src/GummyBears.Contracts/UserProfile.cs
@@ -6,7 +6,7 @@ namespace GummyBears.Contracts
     public class UserProfile : UserProfileBrief
     {
         [Required(AllowEmptyStrings =false, ErrorMessage ="Username must be specified")]
-        [RegularExpression(@"^[A-Za-z][A-Za-z0-9._]{4,15}$",
+        [RegularExpression(@"^[A-Za-z][A-Za-z0-9._]{3,14}$",
             ErrorMessage = "Username can contain only letters, digits, dots and down slashes. Length of the name must be between 4 and 15 characters")]
         public string UserName { get; set; }
 
